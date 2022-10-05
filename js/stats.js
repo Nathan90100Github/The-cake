76450b7b70bd34b0cd4ab8780cbde87d8caf50ba
Nathan90100Github <94223128+Nathan90100Github@users.noreply.github.com>
2022-10-05 14:41:33 +0200

    The Cake v0.1

--- js/stats.js
+++ js/stats.js
@@ -1,23 +1,46 @@
+//    Main stats
 var messages = 0
 var messagegain = 1
+var clicks = 0
+//    Upgrade stats (messages)
 var upg111 = 0
 var upg112 = 0
 var upg113 = 0
-var clicks = 0
+var upg121 = 0
+var upg122 = 0
+var upg123 = 0
+//    Upgrade stats (total)
+var upgs1 = 0
 
 function log(base, number) {
     return Math.log(number) / Math.log(base);
   }
 function incrementButton() {
     messagegain=1;
+    upgs1=0;
     if (upg111 == 1) {
         messagegain += 2
+        ++upgs1
+    }
+    if (upg122 == 1) {
+        ++upgs1
+        messagegain += (upgs1*3)
     }
     if (upg112 == 1) {
         messagegain *= (Math.round(((Math.log10(messages+1))*100))/100+1)
+        ++upgs1
     }
     if (upg113 == 1) {
         messagegain *= (Math.round(((Math.log10(clicks+1))*110))/100+1)
+        ++upgs1
+    }
+    if (upg123 == 1) {
+        messagegain *= 2
+        ++upgs1
+    }
+    if (upg121 == 1) {
+        ++upgs1
+        messagegain *= Math.pow(1.2,upgs1)
     }
     messages += messagegain
     ++clicks
@@ -50,6 +73,36 @@ function incrementButton() {
             if (upg112 == 1 && upg113 ==0) {
                 messages -= 1000
                 ++upg113
+               document.getElementById('messagecount').innerHTML = messages;
+               document.getElementById('clickmessage').innerHTML = messagegain;
+            }
+        }
+    }
+    function upgrade121() {
+        if (messages >= 3000) {
+            if (upg113 == 1 && upg121 ==0) {
+                messages -= 3000
+                ++upg121
+               document.getElementById('messagecount').innerHTML = messages;
+               document.getElementById('clickmessage').innerHTML = messagegain;
+            }
+        }
+    }
+    function upgrade122() {
+        if (messages >= 10000) {
+            if (upg121 == 1 && upg122 ==0) {
+                messages -= 10000
+                ++upg122
+               document.getElementById('messagecount').innerHTML = messages;
+               document.getElementById('clickmessage').innerHTML = messagegain;
+            }
+        }
+    }
+    function upgrade123() {
+        if (messages >= 50000) {
+            if (upg122 == 1 && upg123 ==0) {
+                messages -= 50000
+                ++upg123
                document.getElementById('messagecount').innerHTML = messages;
                document.getElementById('clickmessage').innerHTML = messagegain;
             }
